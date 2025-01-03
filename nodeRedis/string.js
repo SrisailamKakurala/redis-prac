fb99e01a39e8f527ba59ceb87ed812b17d4b839f
srisailamkakurala <srisailamkakurala@gmail.com>
2025-01-03 22:41:57 +0530

    completed tutorial

--- nodeRedis/string.js
+++ nodeRedis/string.js
@@ -2,6 +2,7 @@ const client = require('./client');
 
 async function init() {
     await client.set('name', 'John Doe');
+    await client.expire('name', 10); // expires in 10 seconds
     const name = await client.get('name');
     console.log(name); // John Doe
 }
